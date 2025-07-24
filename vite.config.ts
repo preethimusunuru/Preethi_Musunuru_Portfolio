feb3d83b058f06da7bd557088d9b4074d913224e
Preethi Musunuru <preethimusunuru@MacBookAir.lan>
2025-07-24 12:40:35 -0400

    Update vite config and setup for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-
 export default defineConfig({
+  base: "/Preethi_Musunuru_Portfolio/", // ✅ GitHub Pages base path
   server: {
     host: "::",
     port: 3000,
   },
   plugins: [
-    react(), // ✅ Only React plugin
+    react(),
   ],
   resolve: {
     alias: {
